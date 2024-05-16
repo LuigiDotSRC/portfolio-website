d92486ec72ce6fb313146bce8b53f0fbfc270ed9
LuigiDotSRC <luigi.terashima@gmail.com>
2024-05-16 10:49:56 -0400

    Add 'contact me' functionality.

--- src/pages/ContactView.jsx
+++ src/pages/ContactView.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import Intro from "../components/Intro";
 import Navbar from "../components/Navbar";
+import Contact from "../components/Contact";
 
 function ContactView() {
   return (
     <div>
       <Intro />
       <Navbar select='contact'/>
-      <div className="flex items-center justify-center">
-        <p className="italic my-8">Coming soon...</p>
-      </div>
+      <Contact />
     </div>
   )
 }
